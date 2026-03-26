8f1d396b3586969ef9d4c4c50e647a7cada65d48
Amy <amyelouisell@gmail.com>
2026-03-26 17:58:36 +0100

    Añadir cartas y prefabs, script de click y on mouse

---
+++ RPMI_P03/Assets/Scripts/CardStats.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CardStats : MonoBehaviour
+{
+    [SerializeField, Tooltip("Ataque de la carta diousa"), Range(1,50)]
+    private int attack;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
